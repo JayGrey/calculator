20498368535d8c345c9c572d5ed7266c78b41944
JayGrey <jaygrey@yandex.ru>
2019-11-08 23:35:53 +0200

    eval wip

--- src/utils.ts
+++ src/utils.ts
@@ -27,74 +27,107 @@ const trimBeginning = (arr: string[]): string[] => {
   }
 };
 
-const findNextOperator = (oper: string, arr: string[]): number => {
-  return -1;
+const findNextOperator = (oper: string, arr: string[], pos = 0): number => {
+  if (pos >= arr.length) {
+    return -1;
+  }
+  return arr[pos][0] == oper ? pos : findNextOperator(oper, arr, pos + 1);
 };
 
 const calculate = (oper: string, num1: string, num2: string): string => {
   const koefecient = oper.length == 2 && oper[1] == "-" ? -1 : 1;
-  switch (oper[0]) {
-    case "+":
-      return Number(Number(num1) + koefecient * Number(num2)).toString();
-    case "-":
-      return Number(Number(num1) - koefecient * Number(num2)).toString();
-    case "*":
-      return Number(Number(num1) * koefecient * Number(num2)).toString();
-    case "/":
-      return Number(Number(num1) / (koefecient * Number(num2))).toString();
-    default:
-      throw Error(`unknown operator: ${oper}`);
-  }
+  const num =
+    oper[0] == "+"
+      ? Number(num1) + koefecient * Number(num2)
+      : oper[0] == "-"
+      ? Number(num1) - koefecient * Number(num2)
+      : oper[0] == "*"
+      ? Number(num1) * koefecient * Number(num2)
+      : oper[0] == "/"
+      ? Number(num1) / (koefecient * Number(num2))
+      : NaN;
+
+  return (Number.isInteger(num) ? num : Number(num).toFixed(4)).toString();
+
+  // switch (oper[0]) {
+  //   case "+":
+  //     return Number(Number(num1) + koefecient * Number(num2)).toString();
+  //   case "-":
+  //     return Number(Number(num1) - koefecient * Number(num2)).toString();
+  //   case "*":
+  //     return Number(Number(num1) * koefecient * Number(num2)).toString();
+  //   case "/":
+  //     return Number(Number(num1) / (koefecient * Number(num2))).toString();
+  //   default:
+  //     throw Error(`unknown operator: ${oper}`);
+  // }
 };
 
 const evalOperator = (oper: string, arr: string[]): string[] => {
   const index = findNextOperator(oper, arr);
 
-  if (index < 1) {
-    return arr;
-  }
+  return index < 1
+    ? arr
+    : evalOperator(oper, [
+        ...arr.slice(0, index - 1),
+        calculate(arr[index], arr[index - 1], arr[index + 1]),
+        ...arr.slice(index + 2),
+      ]);
 };
 
 export const evaluate = (arr: string[]): string => {
   // simplify start of the arr
   // simplify end of the arr
   // reduce to one sequence of operators
+
   // process / operator
   // process * operator
   // process + operator
   // process - operator
+  const result = evalOperator("-", evalOperator("+", evalOperator("*", evalOperator("/", arr))));
+
   // trim fraction part to 4 digits
-  return "";
+  return result[0];
 };
 
 console.log("util started");
 
 // console.log(evaluate(["1", "+", "2"]));
-console.log(findLastNum(["1", "+", "2", "+", "-"]));
-console.log(findLastNum(["+", "-"]));
-console.log(findLastNum([]));
+// console.log(findLastNum(["1", "+", "2", "+", "-"]));
+// console.log(findLastNum(["+", "-"]));
+// console.log(findLastNum([]));
 
-console.log(findNextNum(["1", "+", "2", "+", "-"]));
-console.log(findNextNum(["-", "1", "+", "2", "+", "-"]));
-console.log(findNextNum(["+", "-", "1", "+", "2", "+", "-"]));
-console.log(findNextNum(["-"]));
-console.log(findNextNum([]));
+// console.log(findNextNum(["1", "+", "2", "+", "-"]));
+// console.log(findNextNum(["-", "1", "+", "2", "+", "-"]));
+// console.log(findNextNum(["+", "-", "1", "+", "2", "+", "-"]));
+// console.log(findNextNum(["-"]));
+// console.log(findNextNum([]));
 
 // simplify operators
-console.log(simplifyOperators(["+"]));
-console.log(simplifyOperators(["*", "-", "/"]));
-console.log(simplifyOperators(["*", "/", "-"]));
-console.log(simplifyOperators(["*", "-", "-"]));
+// console.log(simplifyOperators(["+"]));
+// console.log(simplifyOperators(["*", "-", "/"]));
+// console.log(simplifyOperators(["*", "/", "-"]));
+// console.log(simplifyOperators(["*", "-", "-"]));
 
 // calculate
-console.log(calculate("+-", "2", "3"));
-console.log(calculate("+", "2", "3"));
+// console.log(calculate("+-", "2", "3"));
+// console.log(calculate("+", "2", "3"));
 
-console.log(calculate("--", "2", "3"));
-console.log(calculate("-", "2", "3"));
+// console.log(calculate("--", "2", "3"));
+// console.log(calculate("-", "2", "3"));
 
-console.log(calculate("*-", "2", "3"));
-console.log(calculate("*", "2", "3"));
+// console.log(calculate("*-", "2", "3"));
+// console.log(calculate("*", "2", "3"));
 
-console.log(calculate("/-", "2", "3"));
-console.log(calculate("/", "2", "3"));
+// console.log(calculate("/-", "2", "3"));
+// console.log(calculate("/", "2", "3"));
+
+// findNextOperator
+// console.log(findNextOperator("+", ["1", "/", "2", "-", "3"]));
+// console.log(findNextOperator("+", ["1", "/", "2", "+", "3"]));
+// console.log(findNextOperator("-", ["1", "/", "2", "+", "3"]));
+
+// evalOperator
+// console.log(evalOperator("+", ["7", "+", "2", "+-", "3", "/", "2"]));
+
+console.log(evaluate(["7", "+", "2", "+-", "3", "/", "2"]));
